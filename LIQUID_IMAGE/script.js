b43d71ba5cad480126c254d5a22d1cd63a9509dd
yoel <67129044+yoel0@users.noreply.github.com>
2021-01-07 23:21:50 -0800

    [add] LIQUID_IMAGE

---
+++ LIQUID_IMAGE/script.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+	parent: document.querySelector('.img'),
+  intensity: 0.29,
+  // angle: Math.PI / 8, - Experiment for max Distortion
+	image1: './assets/wahid.jpg',
+	image2: './assets/itnan.jpg',
+  displacementImage: './assets/supercat.png',
+});
