f66c8225c32cccb24f561faa8690ff6d9df95861
yoel <67129044+yoel0@users.noreply.github.com>
2021-01-31 22:18:10 -0800

    [add] CSS_WEAVE_PLATES

---
+++ CSS_WEAVE_PLATES/script.js
@@ -0,0 +1,14 @@
+const w = document.getElementsByClassName('w')[0];
+const panels = document.getElementsByClassName('panels');
+
+for (let i = 1; i < 900; i++) {
+  w.innerHTML += "<div class='panels'></div>";
+  const duration = Math.random() * 5;
+  panels[i].style.animationDuration = 2 + duration +'s';
+  panels[i].style.animationDelay = duration +'s';
+};
+
+const section = document.querySelector('section');
+setTimeout(() => {
+  section.classList.add('active')
+}, 14000); // add class active after 14s
