566b4b597c554b6a7914e4b392302b933ad6ff02
yoel <67129044+yoel0@users.noreply.github.com>
2021-02-08 21:07:56 -0800

    [add] CSS_ANIMA

--- CSS_WEAVE_PLATES/script.js
+++ CSS_WEAVE_PLATES/script.js
@@ -1,7 +1,7 @@
 const w = document.getElementsByClassName('w')[0];
 const panels = document.getElementsByClassName('panels');
 
-for (let i = 1; i < 900; i++) {
+for (let i = 1; i < 500; i++) {
   w.innerHTML += "<div class='panels'></div>";
   const duration = Math.random() * 5;
   panels[i].style.animationDuration = 2 + duration +'s';
