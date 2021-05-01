210fc4e4192ac154524507f4fb708cdc39f09ca3
yoel <67129044+yoel0@users.noreply.github.com>
2021-04-30 23:17:31 -0700

    [add] FLY optimize

--- FLY/script.js
+++ FLY/script.js
@@ -1,2 +1,30 @@
-const text = document.querySelector('.text');
-text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+const text = document.querySelector('.text2');
+text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
+
+const animation = anime.timeline({
+	targets: '.text2 span',
+	easing: 'easeInOutExpo',
+	loop: true,
+});
+
+animation.add({
+	rotate: function() {
+    return anime.random(-360,360)
+  },
+  translateX: function() {
+    return anime.random(-90,90)
+  },
+  translateY: function() {
+    return anime.random(-90,90)
+  },
+  duration: 3000,
+  delay: anime.stagger(20),
+})
+
+.add({
+	rotate: 0,
+  translateX: 0,
+  translateY: 0,
+  duration: 2000,
+  delay: anime.stagger(20),
+});
